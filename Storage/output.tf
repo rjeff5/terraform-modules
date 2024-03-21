dd705914ebc378ec8318112f3a08b9c4af956f3e
rjeff5 <rjeffersonimmanuel@gmail.com>
2024-03-21 21:27:23 +0530

    added storage container

--- Storage/output.tf
+++ Storage/output.tf
@@ -1,18 +1,18 @@
-# modules/storage_account/output.tf
+# # modules/storage_account/output.tf
 
-output "storage_account_name" {
-  description = "The name of the created storage account"
-  value       = azurerm_storage_account.storage_account[*].name
-}
+# output "storage_account_name" {
+#   description = "The name of the created storage account"
+#   value       = azurerm_storage_account.storage_account[*].name
+# }
 
-output "storage_account_id" {
-  description = "The ID of the created storage account"
-  value       = azurerm_storage_account.storage_account[*].id
-}
+# output "storage_account_id" {
+#   description = "The ID of the created storage account"
+#   value       = azurerm_storage_account.storage_account[*].id
+# }
 
-output "primary_blob_endpoint" {
-  description = "The primary blob storage endpoint URL"
-  value       = azurerm_storage_account.storage_account[*].primary_blob_endpoint
-}
+# output "primary_blob_endpoint" {
+#   description = "The primary blob storage endpoint URL"
+#   value       = azurerm_storage_account.storage_account[*].primary_blob_endpoint
+# }
 
-# Add more outputs as needed
+# # Add more outputs as needed
