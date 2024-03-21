79d7580f4ab3895eb6b4edba47ad276b38598039
rjeff5 <rjeffersonimmanuel@gmail.com>
2024-03-21 17:45:47 +0530

    Added files for storage

--- Storage/output.tf
+++ Storage/output.tf
@@ -1,16 +1,16 @@
-output "storage_account_id" {
-  description = "ID of the storage account"
-  value       = azurerm_storage_account.storage_account[*].id
-}
+# output "storage_account_id" {
+#   description = "ID of the storage account"
+#   value       = azurerm_storage_account.storage_account[*].id
+# }
 
 
-output "storage_account_name" {
-  description = "Name of the storage account"
-  value       = azurerm_storage_account.storage_account[*].name
-}
+# output "storage_account_name" {
+#   description = "Name of the storage account"
+#   value       = azurerm_storage_account.storage_account[*].name
+# }
 
 
-output "storage_account_connection_string" {
-  description = "Connection string for the storage account"
-  value       = "DefaultEndpointsProtocol=https;AccountName=${azurerm_storage_account.storage_account[*].name};AccountKey=${azurerm_storage_account.storage_account[*].primary_access_key};EndpointSuffix=core.windows.net"
-}
+# output "storage_account_connection_string" {
+#   description = "Connection string for the storage account"
+#   value       = "DefaultEndpointsProtocol=https;AccountName=${azurerm_storage_account.storage_account[*].name};AccountKey=${azurerm_storage_account.storage_account[*].primary_access_key};EndpointSuffix=core.windows.net"
+# }
