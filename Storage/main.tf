b614efddc677917fece5344e46c0d975b28778ea
rjeff5 <rjeffersonimmanuel@gmail.com>
2024-03-21 20:51:28 +0530

    added storage container

--- Storage/main.tf
+++ Storage/main.tf
@@ -1,3 +1,36 @@
+# resource "azurerm_storage_account" "storage_account" {
+#   count = length(var.storage_accounts)
+
+#   name                     = var.storage_accounts[count.index].storage_account_name
+#   resource_group_name      = var.storage_accounts[count.index].resource_group_name
+#   location                 = var.storage_accounts[count.index].location
+#   account_tier             = var.storage_accounts[count.index].account_tier
+#   account_replication_type = var.storage_accounts[count.index].account_replication_type
+
+#   # Other storage account settings
+# }
+
+
+# resource "azurerm_storage_container" "containers" {
+#   count                = length(var.storage_accounts)
+#   name                 = var.containers
+#   storage_account_name = var.storage_accounts[count.index].storage_account_name
+#   container_access_type = "private"
+# }
+
+
+
+resource "azurerm_storage_account" "storage_account" {
+  count = length(var.storage_accounts)
+
+  name                     = var.storage_accounts[count.index].storage_account_name
+  resource_group_name      = var.storage_accounts[count.index].resource_group_name
+  location                 = var.storage_accounts[count.index].location
+  account_tier             = var.storage_accounts[count.index].account_tier
+  account_replication_type = var.storage_accounts[count.index].account_replication_type
+
+  # Other storage account settings
+}
 resource "azurerm_storage_account" "storage_account" {
   count = length(var.storage_accounts)
 
@@ -10,10 +43,10 @@ resource "azurerm_storage_account" "storage_account" {
   # Other storage account settings
 }
 
+resource "azurerm_storage_container" "storage_container" {
+  count = length(var.storage_accounts[count.index].containers)  // Corrected this line
 
-resource "azurerm_storage_container" "containers" {
-  count                = length(var.storage_accounts)
-  name                 = var.containers
-  storage_account_name = var.storage_accounts[count.index].storage_account_name
-  container_access_type = "private"
+  name                  = var.storage_accounts[count.index].containers[count.index].container_name  // Adjusted this line
+  storage_account_name  = azurerm_storage_account.storage_account[count.index].name  // Adjusted this line
+  container_access_type = var.storage_accounts[count.index].containers[count.index].container_access_type  // Adjusted this line
 }
--- Storage/variables.tf
+++ Storage/variables.tf
@@ -1,11 +1,26 @@
+# variable "storage_accounts" {
+#   description = "List of storage account configurations"
+#   type        = list(object({
+#     storage_account_name      = string
+#     resource_group_name       = string
+#     location                  = string
+#     account_tier              = string
+#     account_replication_type  = string
+#     # Add other necessary properties
+#   }))
+# }
 variable "storage_accounts" {
-  description = "List of storage account configurations"
+  description = "A list of storage account configurations."
   type        = list(object({
     storage_account_name      = string
     resource_group_name       = string
     location                  = string
     account_tier              = string
     account_replication_type  = string
-    # Add other necessary properties
+    containers                = list(object({
+      container_name         = string
+      container_access_type  = string
+    }))
+    // Add more properties as needed for storage account configurations
   }))
 }
