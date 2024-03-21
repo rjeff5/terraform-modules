105c357c0f5484da9081e4dbf5d9f7809fc26e91
rjeff5 <rjeffersonimmanuel@gmail.com>
2024-03-21 20:55:02 +0530

    added storage container

--- Storage/main.tf
+++ Storage/main.tf
@@ -31,17 +31,7 @@ resource "azurerm_storage_account" "storage_account" {
 
   # Other storage account settings
 }
-resource "azurerm_storage_account" "storage_account" {
-  count = length(var.storage_accounts)
 
-  name                     = var.storage_accounts[count.index].storage_account_name
-  resource_group_name      = var.storage_accounts[count.index].resource_group_name
-  location                 = var.storage_accounts[count.index].location
-  account_tier             = var.storage_accounts[count.index].account_tier
-  account_replication_type = var.storage_accounts[count.index].account_replication_type
-
-  # Other storage account settings
-}
 
 resource "azurerm_storage_container" "storage_container" {
   count = length(var.storage_accounts[count.index].containers)  // Corrected this line
