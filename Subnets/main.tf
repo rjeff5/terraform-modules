97f1ee1091a05bb8a646a3b8a9a41d32bdde074a
rjeff5 <rjeffersonimmanuel@gmail.com>
2024-03-25 17:59:47 +0530

    added vnet/subnets

--- Subnets/main.tf
+++ Subnets/main.tf
@@ -1,22 +1,3 @@
-# resource "azurerm_subnet" "example" {
-#   count                = length(var.subnets)
-#   name                 = var.subnets[count.index].name
-#   resource_group_name  = var.resource_group_name
-#   virtual_network_name = var.vnet_name
-#   address_prefixes     = var.address_prefixes
-
-# #   tags = var.tags
-# }
-
-
-# resource "azurerm_subnet" "example" {
-#   for_each             = { for idx, subnet_name in var.subnet_names : idx => subnet_name }
-#   name                 = each.value
-#   resource_group_name  = var.resource_group_name
-#   virtual_network_name = var.vnet_name
-#   address_prefixes     = var.address_prefixes
-# }
-
 
 resource "azurerm_subnet" "example" {
   for_each             = var.subnet_names
